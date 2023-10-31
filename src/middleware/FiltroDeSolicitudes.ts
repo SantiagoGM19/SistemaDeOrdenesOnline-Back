3b327c07c0e1e312e42cdcdd3f47f45a7cb37f52
BilbaoJ <jessicabilbao02@gmail.com>
2023-10-30 23:31:31 -0500

    Implementacion de filtro de ip con solicitudes fallidas

--- src/middleware/FiltroDeSolicitudes.ts
+++ src/middleware/FiltroDeSolicitudes.ts
@@ -1,7 +1,36 @@
 export default class FiltroDeSolicitudes extends Verificador{
+
+    private conteoIPRequest: Map<string, number> = new Map();  
+    private ipBloqueadas: Map<string, number> = new Map();
+    private intentosMaximos: number = 3;
+    private tiempoMaximoBloqueo: number = 60000; //milisegundos
     
     verificar(infoSolicitud: any) {
-        throw new Error("Method not implemented.");
+
+        const ip = infoSolicitud.connection.remoteAddress;
+        const intentosRequest = this.conteoIPRequest.get(ip) || 0;
+
+        if (intentosRequest >= this.intentosMaximos) {
+            const tiempoActual = Date.now();
+            const tiempoBloqueo = this.ipBloqueadas.get(ip+'_bloqueada');
+            if (!tiempoBloqueo) {
+                this.bloquearIP(ip, tiempoActual);
+            }else if (tiempoActual-tiempoBloqueo >= this.tiempoMaximoBloqueo){
+                this.limpiarBloqueo(ip);
+            }else{
+                throw new Error('Esta IP está bloqueada');
+            }
+        } else {
+            this.conteoIPRequest.set(ip, intentosRequest + 1);
+        }
     }
-    
+
+    bloquearIP(ip: string, tiempoActual:number){
+        this.ipBloqueadas.set(ip+'_bloqueada', tiempoActual);
+    }
+
+    limpiarBloqueo(ip: string){
+        this.conteoIPRequest.set(ip, 1);
+        this.ipBloqueadas.delete(ip);
+    }
 }
